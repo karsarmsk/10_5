62bb0c9be9b75076470a23a18f5740e7a10ddebe
Сергей Карасев <karasev-s@internet.ru>
2024-09-21 22:41:22 +0300

    Add files via upload

---
+++ 10_5.py
@@ -0,0 +1,29 @@
+import multiprocessing
+import datetime
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line)
+
+if __name__ == '__main__':
+
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    start = datetime.datetime.now()
+    for filename in filenames:
+        read_info(filename)
+    end = datetime.datetime.now()
+    print(end - start)
+#0:00:05.393613
+    # start = datetime.datetime.now()
+    # with multiprocessing.Pool(processes=4) as pool:
+    #     pool.map(read_info, filenames)
+    # end = datetime.datetime.now()
+    # print(end - start)
+#0:00:02.010596
